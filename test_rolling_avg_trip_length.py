7185275917072929c7fe39907261365779583923
learnere <zhangpei1211@gmail.com>
2020-01-04 00:07:02 +0100

    test time data type

---
+++ test_rolling_avg_trip_length.py
@@ -0,0 +1,24 @@
+import unittest
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+from rolling_avg_trip_length import time_series_index_by_col
+
+col = 'VendorID'
+test_file_name = 'data/test/test_input'
+data = pd.read_csv(test_file_name)
+input_df = pd.DataFrame(data,columns=['VendorID', 'trip_distance','tpep_pickup_datetime'])
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_time_series_data_type(self):
+        datetime_col = 'pickup_datetime'
+        time_series_data = time_series_index_by_col(data,datetime_col)
+        self.assertEqual('datetime64[ns]',time_series_data.index.dtype)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
