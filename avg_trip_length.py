305aeb685e0a51df2f7d48cb930c7cb25a150a3f
learnere <zhangpei1211@gmail.com>
2020-01-04 01:20:46 +0100

    draft

--- avg_trip_length.py
+++ avg_trip_length.py
@@ -2,22 +2,21 @@
 """
 -------------------------------------------------
    File Name：     avg_trip_length
-   Description :   calculate the average trip length
+   Description :
    Author :        pzhang01
-   date：          02/01/2020:
+   date：          04/01/2020:
 -------------------------------------------------
    Change Activity:
-                   02/01/2020:
+                   04/01/2020:
 -------------------------------------------------
 """
 __author__ = 'pzhang01'
 import pandas as pd
 
-
-# test_file = 'data/input/yello_data.csv'
-# file = 'data/input/yellow_tripdata_2019-01.csv'
-# ingest_file = 'data/input/new_data.csv'
 test = 'data/test/test_input'
+data_file = 'data/input/yellow_tripdata_2019-01.csv'
+
+#test functions
 
 def get_id_counts_by_col(dataframe,col):
     return dataframe.groupby([col]).size().reset_index(name='id_counts')
@@ -25,11 +24,32 @@ def get_sum_dist_by_col(dataframe,col):
     return dataframe.groupby([col]).sum().unstack().reset_index(name='sum_distance')
 def merge_idCounts_sumDist_by_col(id_counts, sum_dist, col):
     return pd.merge(id_counts,sum_dist, how='inner', on = col).drop(['level_0'], axis=1)
-
 def avg_dist(merged_df):
     avg_distance = pd.DataFrame(merged_df['sum_distance']/merged_df['id_counts'], columns=['avg_distance'])
     return merged_df.join(avg_distance)
+#
 
+def get_data(file_path):
+    id =[]
+    distance = []
+    pickup_datetime = []
+    # dtype={'VendorID': 'int64', 'trip_distance': 'float64','tpep_pickup_datetime':'object'},
+    chunk_iter = pd.read_csv(test,chunksize=100000, usecols=['VendorID', 'trip_distance','tpep_pickup_datetime'])
+    for chunk in chunk_iter:
+        id.append(chunk['VendorID'])
+        distance.append(chunk['trip_distance'])
+        pickup_datetime.append(chunk['tpep_pickup_datetime'])
+    vendor_id = pd.concat(id)
+    trip_distance = pd.concat(distance)
+    tpep_pickup_datetime = pd.concat(pickup_datetime)
+    # rename columes
+    data= pd.DataFrame({'id': vendor_id, 'distance':trip_distance, 'pickup_datetime': tpep_pickup_datetime})
+    return data
+
+def check_null(dataframe):
+    '''check how many null data are there'''
+    print('\n****check null value****\n')
+    return print(dataframe.isnull().sum())
 
 def cal_avg_trip_length_by_col(dataframe,col):
     id_counts = dataframe.groupby([col]).size().reset_index(name='id_counts')
@@ -42,48 +62,9 @@ def cal_avg_trip_length_by_col(dataframe,col):
     # calculate the average distance
     return merged_df.join(avg_distance)
 
-def check_null(dataframe):
-    '''check how many null data are there'''
-    print('\n****check null value****\n','The number of null value = ')
-    return dataframe.isnull().sum()
-
-id =[]
-distance = []
-pickup_datetime = []
-# dtype={'VendorID': 'int64', 'trip_distance': 'float64','tpep_pickup_datetime':'object'},
-chunk_iter = pd.read_csv(test,chunksize=100000, usecols=['VendorID', 'trip_distance','tpep_pickup_datetime'])
-for chunk in chunk_iter:
-    id.append(chunk['VendorID'])
-    distance.append(chunk['trip_distance'])
-    pickup_datetime.append(chunk['tpep_pickup_datetime'])
-vendor_id = pd.concat(id)
-trip_distance = pd.concat(distance)
-tpep_pickup_datetime = pd.concat(pickup_datetime)
-data= pd.DataFrame({'id': vendor_id, 'distance':trip_distance, 'pickup_datetime': tpep_pickup_datetime})
-print(data)
-print('*')
-# data.set_index('pickup_datetime')
-print(cal_avg_trip_length_by_col(data,'id'))
-
-col ='id'
-# print('id_counts')
-# id_counts = get_id_counts_by_col(data,col)
-# print(id_counts)
-#
-# print('sum_dist')
-# sum_dist = get_sum_dist_by_col(data,col)
-# print(sum_dist)
-# print(sum_dist.shape)
-#
-# print('merge')
-# merge = merge_idCounts_sumDist_by_col(id_counts,sum_dist,col)
-# print(merge)
 
 
-
-
-
-
-
-
-
+data = get_data(test)
+check_null(data)
+col = 'id'
+print(cal_avg_trip_length_by_col(data,col))
